fac8cb1ecda9f65f342a9c29eff3192abdee94f0
Cody Michaud <cody.r.michaud@gmail.com>
2021-02-21 22:49:01 -0800

    downloaded inquirer. coded constructors for generate, fs and inquirer. coded prompts for user

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { generate } = require("rxjs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Whats your Github username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'project',
+            message: 'What is your projects name?',
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'Write a brief description of your project.',
+        },
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
@@ -11,3 +40,4 @@ function init() { }
 
 // Function call to initialize app
 init();
+questions();
